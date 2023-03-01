217d2b32d76579836bcd687d51a60202d3146b3f
Sidharth Narayanan <sidharth.narayanan@live.com>
2023-03-01 11:11:08 -0600

    Rotational acc on dashboard

--- src/main/java/frc/robot/interfaces/DriveController.java
+++ src/main/java/frc/robot/interfaces/DriveController.java
@@ -3,6 +3,7 @@ package frc.robot.interfaces;
 import com.revrobotics.RelativeEncoder;
 
 public interface DriveController {
+    public final String ROTATIONAL_ACC_LIMIT="Rot Acc Limt";
     public void move(double speed, double rotation);
     public void stop();
     public RelativeEncoder getRighttEncoder();
