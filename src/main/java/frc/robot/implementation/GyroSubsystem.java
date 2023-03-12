0b24770c1d6d2b6b2b53d4c9abebab1bda8ef0d9
Sidharth Narayanan <sidharth.narayanan@live.com>
2023-03-12 18:01:40 -0500

    navx dependency

--- src/main/java/frc/robot/implementation/GyroSubsystem.java
+++ src/main/java/frc/robot/implementation/GyroSubsystem.java
@@ -1,22 +1,27 @@
 package frc.robot.implementation;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.SPI;
 
 public class GyroSubsystem {
-    
-    private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-    //AHRS gyro = new AHRS(SPI.Port.kMXP);
+    private ADXRS450_Gyro gyro1 = new ADXRS450_Gyro();
+    AHRS ahrsGyro = new AHRS(SPI.Port.kMXP);
+
+    GyroSubsystem() {
+        ahrsGyro.reset();
+    }
 
     public double getAngle() {
-        return gyro.getAngle();
+        return ahrsGyro.getAngle();
     }
 
     public double getRate() {
-        return gyro.getRate();
+        return ahrsGyro.getRate();
     }
 
     public double getDegrees() {
-        return gyro.getRotation2d().getDegrees();
+        return ahrsGyro.getRotation2d().getDegrees();
     }  
 }
